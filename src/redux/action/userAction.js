0f2f83257f516e1c06c8862ec6c8ae279a2e9d93
ayushimastro <amrikmastro@gmail.com>
2021-07-29 11:43:04 +0530

    api integration

--- src/redux/action/userAction.js
+++ src/redux/action/userAction.js
@@ -5,15 +5,15 @@ export const registerUser = params => async dispatch => {
   const resp = await userApis.registerUser(params);
   dispatch({
     type: userActionTypes.RegisterUser,
-    payload: resp.data,
+    payload: resp,
   });
-  return resp.data;
+  return resp;
 };
 export const LoginUser = params => async dispatch => {
   const resp = await userApis.LoginUser(params);
   dispatch({
     type: userActionTypes.LoginUser,
-    payload: resp.data,
+    payload: resp,
   });
-  return resp.data;
+  return resp;
 };
